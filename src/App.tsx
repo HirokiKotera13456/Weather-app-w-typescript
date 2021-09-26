9e9968b84006d2c80beac51031cf0d2e223d02a8
nijimajohn <86758487+nijimajohn@users.noreply.github.com>
2021-09-26 10:07:22 +0900

    [typescript導入]

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import Title from './components/Title';
+import Form from './components/Form';
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    
+      <Title />
+      <Form />
     </div>
   );
-}
+};
 
 export default App;
